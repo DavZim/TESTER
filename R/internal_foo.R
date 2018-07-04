994b7c9d80fcf743be8c2b4c0f8d97c79be07cbc
David <david-zimmermann@hotmail.de>
2018-07-04 13:25:16 +0200

    added large data.frame-object to internal-foo

---
+++ R/internal_foo.R
@@ -0,0 +1,20 @@
+#' Uses future_lapply internally
+#'
+#' @return invisible null
+#' @export
+#'
+#' @examples
+#' internal_foo()
+internal_foo <- function() {
+  n <- 1e7
+  df <- data.frame(x = rnorm(n),
+                   y = rnorm(n),
+                   grp = sample(LETTERS, n, replace = T))
+
+  t0 <- Sys.time()
+  a <- future.apply::future_lapply(split(df, df$grp), function(d) {
+    Sys.sleep(0.1)
+    nrow(d)
+  })
+  cat(sprintf("Time spend on code in seconds: %.2f\n", difftime(Sys.time(), t0, "secs")))
+}
